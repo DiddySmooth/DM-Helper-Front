27fb899b0b49edba440764e51031456bf20942c3
Grayson McClead <gmcclead@mc02yt1kflvdl.attlocal.net>
2021-10-12 20:58:22 -0500

    started town generator

--- dm-helper/src/Pages/Generators/Generators.tsx
+++ dm-helper/src/Pages/Generators/Generators.tsx
@@ -3,29 +3,44 @@ import Npc from '../../Assets/npc.png'
 import MagicShop from '../../Assets/magicShop.png'
 import RandomNpc from './NpcGen/RandomNpc';
 import RandomMagicShop from './MagicShopGen/MagicShop';
+import TownGen from './TownGen/TownGen';
 
 const Generators = () => {
 
     const [npc, setNpc] = useState<boolean>(false)
     const [magicShop, setMagicShop] = useState<boolean>(false)
+    const [town, setTown] = useState<boolean>(false)
 
+    const resetState = () => {
+        setNpc(false)
+        setMagicShop(false)
+        setTown(false)
+
+    }
 
     const clickHandle = (button: string) => {
         if(button === "NPC"){
+            resetState()
             setNpc(true)
-            setMagicShop(false)
         }
         else if(button === "MagicShop"){
+            resetState()
             setMagicShop(true)
-            setNpc(false)
+        }
+        else if(button === "Town"){
+            resetState()
+            setTown(true)
         }
     }
     return(
         <div>
             <img className="genIcon" src={Npc} onClick={ () => {clickHandle("NPC")}}/>
             <img className="genIcon" src={MagicShop} onClick={ () => {clickHandle("MagicShop")}}/>
+            <img className="genIcon" src={Npc} onClick={ () => {clickHandle("Town")}}/>
             {npc && <RandomNpc />}
             {magicShop && <RandomMagicShop />}
+            {town && <TownGen />}
+            
         </div>
     )
 }
---
+++ dm-helper/src/Pages/Generators/TownGen/TownGen.tsx
@@ -0,0 +1,8 @@
+const TownGen = () => {
+    return(
+        <div>
+            Town
+        </div>
+    )
+}
+export default TownGen
