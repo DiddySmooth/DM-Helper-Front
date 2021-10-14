50cec5390eb230085ea916aa42bb74258da415a2
Grayson McClead <gmcclead@mc02yt1kflvdl.attlocal.net>
2021-10-13 21:53:03 -0500

    Started the magic items list

---
+++ dm-helper/src/Pages/Generators/MagicShopGen/Arrays.ts
@@ -0,0 +1,12 @@
+interface MagicItem {
+    name: string;
+    rarity: string;
+    type: string;
+  }
+   
+
+export const MagicItems : MagicItem[] = [
+    {name: "Bag of Holding", rarity: "uncommon", type: "Wondrous"},
+    {name: "Bottle Blade", rarity: "uncommon", type: "Wondrous"},
+
+]
